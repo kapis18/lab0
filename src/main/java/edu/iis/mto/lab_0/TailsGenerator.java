b2fca5821e57fc85b0568731ebd7457921e49101
dębek.kacper <215702@edu.p.lodz.pl>
2020-03-03 18:15:09 +0100

    TailsGenerator edit

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i = 0; i < value.length(); i++){
+            arr.add(value.substring(i));
+        }
+        arr.add("");
+        return arr;
     }
-
 }
